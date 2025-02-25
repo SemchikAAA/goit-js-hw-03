9ef00992fcbf9c46a4bc69bb81b938167023ce5e
Andrii S <semtrololo@gmail.com>
2025-02-25 18:38:20 +0100

    hm_3-v_1

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,25 @@
 'use strict';
 
-function checkForSpam(message) {
-  const uppercaseMesage = message.toUpperCase();
-  if (uppercaseMesage.includes('SPAM') || uppercaseMesage.includes('SALE')) {
-    return true;
-  } else {
-    return false;
+// Напиши функцію filterArray(numbers, value), яка приймає масив чисел (numbers) та значення (value) як параметри. Функція повинна повертати новий масив лише тих чисел із масиву numbers, які більші за значення value.
+
+// Усередині функції:
+// Створи порожній масив, у який будеш додавати підходящі числа.
+// Використай цикл для ітерації кожного елемента масиву numbers.
+// Використовуй умовний оператор if усередині циклу для перевірки кожного елемента и додавання до свого масиву.
+// Поверни свій новий масив з підходящими числами як результат.
+
+function filterArray(numbers, value) {
+  const array = [];
+  for (const element of numbers) {
+    if (element > value) {
+      array.push(element);
+    }
   }
+  return array;
 }
 
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
